976b9877876f6977e7249fa64519f84c06dbe5da
John Burns <wakingrufus@gmail.com>
2026-05-27 10:17:56 -0500

    disable the jetbrains annotation rule
    this creates a ton of noise since kotlin still generates code with this annotation
    I am leaving the rule here, just not added to the rule set, so we can use it later, or if anyone wants to reuse it in their own rule set

--- archrules-nullability/src/archRules/java/com/netflix/nebula/archrules/nullability/NebulaNullabilityArchRules.java
+++ archrules-nullability/src/archRules/java/com/netflix/nebula/archrules/nullability/NebulaNullabilityArchRules.java
@@ -26,8 +26,8 @@ public class NebulaNullabilityArchRules implements ArchRulesService {
             .should(be(nullSafe()))
             .allowEmptyShould(true)
             .because("public classes should be null marked");
+
     static final ArchRule UPGRADE_LEGACY_JETBRAINS = ArchRuleDefinition
-            // kotlin generates code with this annotation still https://youtrack.jetbrains.com/issue/KT-47417/
             .priority(Priority.LOW)
             .noClasses()
             .that(are(nullSafe()))
@@ -75,7 +75,10 @@ public class NebulaNullabilityArchRules implements ArchRulesService {
     public Map<String, ArchRule> getRules() {
         Map<String, ArchRule> rules = new HashMap<>();
         rules.put("public classes should be @NullMarked", PUBLIC_CLASSES_SHOULD_BE_NULL_MARKED);
-        rules.put("upgrade legacy jetbrains annotations", UPGRADE_LEGACY_JETBRAINS);
+
+        // kotlin generates code with this annotation still https://youtrack.jetbrains.com/issue/KT-47417/
+        // rules.put("upgrade legacy jetbrains annotations", UPGRADE_LEGACY_JETBRAINS);
+
         rules.put("upgrade legacy spring annotations", UPGRADE_LEGACY_SPRING_FRAMEWORK);
         rules.put("upgrade legacy javax annotations", UPGRADE_LEGACY_JAVAX);
         rules.put("upgrade legacy jakarta annotations", UPGRADE_LEGACY_JAKARTA);
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.netflix.nebula.oss.settings") version("25.+")
+    id("com.netflix.nebula.oss.settings") version("25.11.+")
 }
 
 rootProject.name = "nebula-archrules"
